be36c2e85e4c59b80054b0dfaba711c03f4a8122
Yoruniii <cyberkicia@gmail.com>
2022-01-23 07:54:23 +0100

    Initial commit

---
+++ src/main/java/net/yoruniii/librariantweaks/LibrarianTweaks.java
@@ -0,0 +1,12 @@
+package net.yoruniii.librariantweaks;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LibrarianTweaks implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+	}
+}
